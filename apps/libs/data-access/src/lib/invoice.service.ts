1d530635c9ccf8f559ed629f958eb4eed2f7ea05
CarlosCastilloPlexus <carlos.castillomartinez@plexus.es>
2025-04-14 14:05:45 +0200

    feat: create invoice service

---
+++ apps/libs/data-access/src/lib/invoice.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Invoice } from '@org/core';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+  constructor(private http: HttpClient) { }
+
+  getInvoices(): Observable<Invoice[]> {
+    return this.http.get<Invoice[]>('/assets/data.json');
+  }
+}
