9791336afc752889c4bd498452b4ff626e550a7a
Cristian González Vélez <mr.bombadil@gmail.com>
2025-04-23 16:24:35 +0200

    fix: INVOICE_REPOSITORY name issue

--- apps/libs/invoices/src/lib/ports/invoice.port.token.ts
+++ apps/libs/invoices/src/lib/ports/invoice.port.token.ts
@@ -2,7 +2,7 @@ import { inject, InjectionToken } from '@angular/core';
 import { InvoiceRepository } from './invoice.port';
 import { InvoiceHttpRepository } from '../infra/repositories/invoice-http.repository';
 
-export const CART_REPOSITORY = new InjectionToken<InvoiceRepository>(
+export const INVOICE_REPOSITORY = new InjectionToken<InvoiceRepository>(
   'InvoiceRepository',
   {
     providedIn: 'root',
